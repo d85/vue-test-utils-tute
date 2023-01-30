41d926bf50f434828d1b9eaed68904a2107757a2
Dan Carlier <dan.carlier@gmail.com>
2023-01-30 14:17:43 +1100

    Tests for odd and even count

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -17,12 +17,22 @@ const App = {
   `
 }
 
-test('App', () => {
-  const wrapper = mount(App, {
-    props: {
-      count: 1
-    }
+function factory(props) {
+  return mount(App, {
+    props
   })
-  console.log(wrapper.html())
-  expect(wrapper.html()).toContain('Count: 1. Count is odd.')
+}
+
+describe('App', () => {
+  it('render count when even', () => {
+    const wrapper = factory({ count: 2 })
+
+    expect(wrapper.html()).toContain('Count: 2. Count is even.')
+  })
+
+  it('render count when odd', () => {
+    const wrapper = factory({ count: 1 })
+
+    expect(wrapper.html()).toContain('Count: 1. Count is odd.')
+  })
 })
